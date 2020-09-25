d9970468c13addac030422e259d09c507589b718
Lucas Goldner <lucas.goldner@googlemail.com>
2020-09-25 18:23:44 +0200

    Updates

---
+++ precache-manifest.2ab0543459357f9accdfeaf5c095eba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9be96c48b4ecbcefab9925e48ed7f03",
+    "url": "/demon-program/index.html"
+  },
+  {
+    "revision": "4f7cb08b1a335a60ec43",
+    "url": "/demon-program/static/css/main.ab8661da.chunk.css"
+  },
+  {
+    "revision": "8fea1175bb3f195a126a",
+    "url": "/demon-program/static/js/2.48fe235d.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/demon-program/static/js/2.48fe235d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7cb08b1a335a60ec43",
+    "url": "/demon-program/static/js/main.d2514cc4.chunk.js"
+  },
+  {
+    "revision": "8653a16120421aceeb40",
+    "url": "/demon-program/static/js/runtime-main.b64a2b34.js"
+  }
+]);
