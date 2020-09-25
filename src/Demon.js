5be4a2787410fa95a60f88680ec6c585baeb7a4e
Lucas Goldner <lucas.goldner@googlemail.com>
2020-09-25 14:58:24 +0200

    Random dandom mandom fuction works!

--- src/Demon.js
+++ src/Demon.js
@@ -1,33 +1,129 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
-    BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    BarChart, Bar, XAxis, YAxis, Tooltip, Legend,
   } from 'recharts';
+
+const DemonName = styled.h1`
+  font-size: 1.5em;
+  margin-left: -100px;
+  margin-top: -50px;
+`
+
+const DemonData = [
+  {
+    name: "Jack Frost", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Jack-Frost.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Pyro-Jack", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Pyro-Jack.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  },
+  {
+    name: "Black-Frost", level: 5, Hp: 214, Sp: 129, Str: 12, Ma: 22, End: 11, Ag: 13, Luck:10, Exp: 28,
+    img: "./Demon-Designs/Black-Frost.png",
+    weakness: ["Fire"],
+    resist: [],
+    null: ["Ice"],
+    skills: ["Bufu", "Mabufu", "Bufula", "Mabufula", "Ice Boost", "Freeze Boost", "Marakunda", "Dodge Fire", "Me Patra", "Ice Break"]
+  }
+]
 
 const data = [
     {
-      name: 'HP', points: 214,
+      name: 'HP', Points: 214,
     },
     {
-      name: 'SP', points: 129,
+      name: 'SP', Points: 129,
     },
     {
-      name: 'Str', points: 12,
+      name: 'Str', Points: 12,
     },
     {
-      name: 'Ma', points: 22,
+      name: 'Ma', Points: 22,
     },
     {
-      name: 'End', points: 11,
+      name: 'End', Points: 11,
     },
     {
-      name: 'Ag', points: 13,
+      name: 'Ag', Points: 13,
     },
     {
-      name: 'Luck', points: 10,
+      name: 'Luck', Points: 10,
     },
   ];
-  
   
   const getIntroOfPage = (label) => {
     if (label === 'HP') {
@@ -38,6 +134,8 @@ const data = [
       return "Strength partly determines the damage the demon´s physical skills deal";
     } if (label === 'Ma') {
       return 'Magic partly determines the power of magic skills, and the power of recovery skills';
+    }if (label === 'End') {
+      return "Endurance partly determines how much damage the party member can reduce when they are hit by another entity's attack, whether its Physical or Magic.";
     } if (label === 'Ag') {
       return 'Agility mainly determines the turn order between entities, with faster characters acting earlier';
     } if (label === 'Luck') {
@@ -72,17 +170,59 @@ const Stats = styled.div`
 `
 
 const Demon = ({isVisible}) =>{
+    const RandomDemon = (DemonData, prop) =>{
+      let randomIndex = Math.floor(Math.random()*10);
+      if(prop==="img"){
+        return DemonData[randomIndex].img;
+      } else if(prop==="name") {
+        return DemonData[randomIndex].name;
+      } else if(prop==="stats") {
+        let curHP = DemonData[randomIndex].Hp;
+        let curSP = DemonData[randomIndex].Sp;
+        let curStr = DemonData[randomIndex].Str;
+        let curMa = DemonData[randomIndex].Ma;
+        let curEnd = DemonData[randomIndex].End;
+        let curAg = DemonData[randomIndex].Ag;
+        let curLuck = DemonData[randomIndex].Luck;
+        let newData = [];
+        return newData = [
+          {
+            name: 'HP', Points: curHP,
+          },
+          {
+            name: 'SP', Points: curSP,
+          },
+          {
+            name: 'Str', Points: curStr,
+          },
+          {
+            name: 'Ma', Points: curMa,
+          },
+          {
+            name: 'End', Points: curEnd,
+          },
+          {
+            name: 'Ag', Points: curAg,
+          },
+          {
+            name: 'Luck', Points: curLuck,
+          },
+        ];
+
+      }
+    }
     return(
         <>
-        <DemonPic src="./Demon-Designs/Jack-Frost.png" style={{visibility: isVisible ? "visible" : "hidden"}}></DemonPic>
+        <DemonPic src={RandomDemon(DemonData, "img")} style={{visibility: isVisible ? "visible" : "hidden"}}></DemonPic>
         <Stats>
-        <BarChart style={{visibility: isVisible ? "visible" : "hidden"}} width={350} height={200} data={data}
+        <DemonName style={{visibility: isVisible ? "visible" : "hidden"}}>{RandomDemon(DemonData, "name")}</DemonName>
+        <BarChart style={{visibility: isVisible ? "visible" : "hidden"}} width={350} height={200} data={RandomDemon(DemonData, "stats")}
         margin={{top: 5, right: 30, left: 20, bottom: 5,}}>
-        <XAxis dataKey="name" />
-        <YAxis />
+        <XAxis dataKey="name" stroke="white"/>
+        <YAxis stroke="white"/>
         <Tooltip content={<CustomTooltip />} />
         <Legend />
-        <Bar dataKey="points" barSize={20} fill="#F64C72" />
+        <Bar dataKey="Points" barSize={20} fill="#F64C72" />
       </BarChart>
       </Stats>
         </>
