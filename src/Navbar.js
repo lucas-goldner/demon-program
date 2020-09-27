627e315161434ef5748907aa4e4d15b5c8993a14
Lucas Goldner <lucas.goldner@googlemail.com>
2020-09-27 18:11:17 +0200

    Jam packed login screen Update

--- src/Navbar.js
+++ src/Navbar.js
@@ -40,11 +40,42 @@ const ListLogin = styled.button`
 `
 
 const Login = styled.div`
-    background: #111;
+    border-color: #553D67;
+    border-style: solid;
     position: absolute;
-    top: 50px;
+    background: #F64C72;
+    color: #553D67;
+    top: 55px;
+    left: 320px;
     z-index: 2;
-    font-size: 3em;
+    font-size: 1em;
+    display: flex;
+    flex-direction: column;
+`
+
+const InputFields = styled.input`
+    size = 10;
+`
+
+const LoginError = styled.p`
+    font-size: 0.5em;
+    color: #2F2FA2;
+`
+
+const LoginButton = styled.button`
+    color: white;
+    background-color: #553D67; 
+    border-radius: 10px;
+    border-style: none;
+`
+
+const LoginHint = styled.p`
+    font-size: 0.8em;
+`
+
+const LoginSpan = styled.span`
+    color: #2F2FA2;
+    text-decoration: underline;
 `
 
 const Navbar = () => {
@@ -133,20 +164,20 @@ const Navbar = () => {
                     </>
                 ) : (
                     <>
-                    <label>Email</label> <input type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)}></input>
-                    <p>{emailError}</p>
-                    <label>Password</label> <input type="text" required value={password} onChange={e => setPassword(e.target.value)}></input>
-                    <p>{passwordError}</p>
+                    <label>Email</label> <InputFields type="text" autoFocus required value={email} onChange={e => setEmail(e.target.value)}></InputFields>
+                    <LoginError>{emailError}</LoginError>
+                    <label>Password</label> <InputFields type="text" required value={password} onChange={e => setPassword(e.target.value)}></InputFields>
+                    <LoginError>{passwordError}</LoginError>
                     <div>
                     {hasAccount ? (
                         <>
-                            <button onClick={handleLogin}>Sign In</button>
-                            <p>Don´t have an account ? <span onClick={()=>setHasAccount(!hasAccount)}>Sign Up</span></p>
+                            <LoginButton onClick={handleLogin}>Sign In</LoginButton>
+                            <LoginHint>Don´t have an account ? <LoginSpan onClick={()=>setHasAccount(!hasAccount)}>Sign Up</LoginSpan></LoginHint>
                         </>
                     ) : (
                         <>
-                            <button onClick={handleSignUp}>Sign Up</button>
-                            <p>Have an account ? <span onClick={()=>setHasAccount(!hasAccount)}>Sign In</span></p>
+                            <LoginButton onClick={handleSignUp}>Sign Up</LoginButton>
+                            <LoginHint>Have an account ? <LoginSpan onClick={()=>setHasAccount(!hasAccount)}>Sign In</LoginSpan></LoginHint>
                         </>
                     )}
                     </div>
